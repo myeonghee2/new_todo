7bed1653f8d03f64fb5d5928eb960396907dad70
sim myeonghee <mang1670@gmail.com>
2023-09-07 16:12:21 +0900

    add todo create

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from todo.models import Todo
 
 # Create your views here.
+@login_required(login_url='/user/login/')
+@csrf_exempt
+def create(request):
+    if request.method == "POST":
+        Todo.objects.create(
+            content=request.POST["content"], 
+            user=request.user,
+            image=request.FILES.get("image"),
+            )
+        return redirect("/todo/")
+    elif request.method =="GET":
+        return render(request, 'todo/create.html')
+    else:
+        return HttpResponse("Invalid request method", setatus=405)
